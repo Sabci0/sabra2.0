2a50de6e6b5fd8ef10ea3efd4ce74d0483524166
Sabci0 <barteksabcio@gmail.com>
2026-02-07 10:52:27 +0100

    contact info

---
+++ src/config/contact.ts
@@ -0,0 +1,16 @@
+export const CONTACT_INFO = {
+    email: 'kontakt@sabra.pl',
+    phone: '+48 123 456 789',
+    phoneRaw: '+48123456789', // bez spacji do href="tel:"
+    address: {
+      street: 'ul. Przykładowa 123',
+      postalCode: '00-000',
+      city: 'Miejscowość',
+      country: 'Polska'
+    },
+    social: {
+      facebook: 'https://facebook.com/example',
+      instagram: 'https://instagram.com/example',
+      linkedin: 'https://linkedin.com/company/example'
+    }
+  } as const;
